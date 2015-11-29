fce3a35c1b2cf93a41e7cd903d3aebaa42057b1c
Ristomatti Airo <ristomatti@kallio.io>
2015-11-29 05:14:28 +0200

    Add "list" command

--- cli.js
+++ cli.js
@@ -2,67 +2,25 @@
 
 'use strict';
 
-require('app-module-path').addPath(__dirname);
+const vorpal = require('vorpal')();
+const lifxsh = require('./lib/lifxsh');
 
-let LifxClient = require('node-lifx').Client;
-let client = new LifxClient();
-
-client.on('error', function(err) {
-  console.error('Error:\n' + err.stack);
-  client.destroy();
-});
-
-startMessageLogging();
-
-client.on('light-new', function(light) {
-  console.log('New light found. ID:' + light.id + ', IP:' + light.address + ':' + light.port);
-});
-
-client.on('light-online', function(light) {
-  console.log('Light back olifxshnline. ID:' + light.id + ', IP:' + light.address + ':' + light.port);
-});
-
-client.on('light-offline', function(light) {
-  console.log('Light offline. ID:' + light.id + ', IP:' + light.address + ':' + light.port);
-});
-
-client.on('listening', function() {
-  let address = client.address();
-  console.log('Started listening on %s:%d\n', address.address, address.port);
-});
-
-client.init({}, function() {
-  client.lights().forEach(function(light) {
-    console.log('------------------');
-    console.log(light);
+vorpal
+  .command('list', 'Lists connected lights.')
+  .action( (args, cb) => {
+    lifxsh.list();
+    cb();
   });
-});
 
-//console.log(client.prototype.lights());
-
-function startMessageLogging() {
-  client.on('message', function (msg, rinfo) {
-    if (typeof msg.type === 'string') {
-      // Known packages send by the lights as broadcast
-      switch (msg.type) {
-        case 'echoResponse':
-        case 'getOwner':
-        case 'stateOwner':
-        case 'getGroup':
-        case 'getVersion':
-        case 'stateGroup':
-        case 'getLocation':
-        case 'stateLocation':
-        case 'stateTemperature':
-        case 'statePower':
-          //console.log(msg, ' from ' + rinfo.address);
-          break;
-        default:
-          break;
-      }
-    } else {
-      // Unknown message type
-      console.warn(msg, ' from ' + rinfo.address);
-    }
+vorpal.find('exit')
+  .description('Exits lifxsh.')
+  .action( (args, cb) => {
+    lifxsh.disconnect();
+    process.exit(0);
   });
-}
+
+vorpal
+  .delimiter('lifx> ')
+  .show();
+
+lifxsh.connect();
