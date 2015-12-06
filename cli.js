7c887b4b540f6ea294489eec7c0c89be9243d22f
Ristomatti Airo <ristomatti@kallio.io>
2015-12-06 22:58:03 +0200

    Add "on" and "off" commands
    - clean up logging code
    - add JSDoc

--- cli.js
+++ cli.js
@@ -7,14 +7,31 @@ const lifxsh = require('./lib/lifxsh');
 
 vorpal
   .command('list', 'Lists connected lights.')
-  .action( (args, cb) => {
+  .action((args, cb) => {
     lifxsh.list();
     cb();
   });
 
-vorpal.find('exit')
+vorpal
+  .command('on <name>')
+  .description('Turns light on.')
+  .action((args, cb) => {
+    lifxsh.on(args.name);
+    cb();
+  });
+
+vorpal
+  .command('off <name>')
+  .description('Turns light off.')
+  .action((args, cb) => {
+    lifxsh.off(args.name);
+    cb();
+  });
+
+vorpal
+  .find('exit')
   .description('Exits lifxsh.')
-  .action( (args, cb) => {
+  .action((args, cb) => {
     lifxsh.disconnect();
     process.exit(0);
   });
