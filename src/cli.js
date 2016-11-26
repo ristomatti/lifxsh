7c1f93975c00d2cf235251c1582050bec95a3bc0
Ristomatti Airo <ristomatti@kallio.io>
2016-11-26 18:20:09 +0200

    Version 0.6.1
    - default to 'all' if no light name is given

--- src/cli.js
+++ src/cli.js
@@ -26,27 +26,29 @@ vorpal
   });
 
 vorpal
-  .command('on <names...>', 'Turn light(s) on.')
+  .command('on [names...]', 'Turn light(s) on.')
   .option('-d, --duration [ms]', 'Duration (ms)')
   .autocompletion(lightNameAutocompletion)
   .action((args, cb) => {
     let opts = args.options;
-    lifxsh.on(args.names, opts.duration);
+    let names = getLightNames(args.names);
+    lifxsh.on(names, opts.duration);
     cb();
   });
 
 vorpal
-  .command('off <names...>', 'Turn light(s) off.')
+  .command('off [names...]', 'Turn light(s) off.')
   .option('-d, --duration [ms]', 'Duration (ms)')
   .autocompletion(lightNameAutocompletion)
   .action((args, cb) => {
     let opts = args.options;
-    lifxsh.off(args.names, opts.duration);
+    let names = getLightNames(args.names);
+    lifxsh.off(names, opts.duration);
     cb();
   });
 
 vorpal
-  .command('color <names...>', 'Change color of light(s).')
+  .command('color[names...]', 'Change color of light(s).')
   .option('-h, --hue [value]', 'Hue (0-360)')
   .option('-s, --saturation [value]', 'Saturation (0-100)')
   .option('-b, --brightness [value]', 'Brightness (0-100)')
@@ -61,7 +63,8 @@ vorpal
       brightness: opts.brightness,
       kelvin: opts.kelvin
     };
-    lifxsh.color(args.names, color, opts.duration);
+    let names = getLightNames(args.names);
+    lifxsh.color(names, color, opts.duration);
     cb();
   });
 
@@ -72,6 +75,16 @@ vorpal
     lifxsh.disconnect();
     process.exit(0);
   });
+
+/**
+ * Default to 'all' if no light name is provided.
+ *
+ * @param {any} lightNames
+ * @returns
+ */
+function getLightNames(lightNames) {
+  return lightNames ? lightNames : ['all'];
+}
 
 /**
  * If saturation is omitted, use 0% saturation when kelvin is defined, 100% if not.
